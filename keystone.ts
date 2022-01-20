5d848859ad2d8d00843e390ffa39b7ae03466511
Alejandro Oviedo <alexoviedo999@gmail.com>
2022-01-19 20:53:48 -0600

    first commit-adding backend functionality for the duds-store-app-front-end using Mongoose and Keystone

---
+++ keystone.ts
@@ -0,0 +1,56 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'password',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      isAccessAllowed: ({ session }) => {
+        console.log('session: ', session);
+        return !!session?.data;
+      },
+    },
+    // GraphQL Query
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  })
+);
